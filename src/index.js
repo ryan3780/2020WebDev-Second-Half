9309d59f806a6e946c24f6a6939d3c2a513abceb
ryan3780 <45477679+ryan3780@users.noreply.github.com>
2020-08-27 23:30:37 +0900

    마우스 클릭 이벤트 완료/ input value 변경 시 nowpositin 초기화 해야함

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const $keyword = document.querySelector(".keyword");
 const $keywords = document.querySelector(".keywords");
 const $searchResults = document.querySelector(".search-results");
+const spinner = document.getElementById("spinner");
 
 
 let nowPosition = 0;
@@ -10,17 +11,21 @@ $keyword.addEventListener("keyup", (e) => {
 const {value} = e.target;
 const { key } = e;
 
-
   if (key === "Enter") {
     if(choosedText !== ''){
+      nowPosition = 0;
       $keyword.value = choosedText;
       $keywords.style.display = 'none';
-      console.log($keyword.value)
+      spinner.removeAttribute("hidden");
       fetch(
         `https://jf3iw5iguk.execute-api.ap-northeast-2.amazonaws.com/dev/api/cats/search?q=${$keyword.value}`
       )
         .then((res) => res.json())
+        .catch((error) => {
+          console.log(error)
+        })
         .then((results) => {
+          spinner.setAttribute("hidden", "");
           if (results.data) {
             $searchResults.innerHTML = results.data
               .map((cat) => `<article><img src="${cat.url}" /></article>`)
@@ -30,47 +35,79 @@ const { key } = e;
     }
   }else{
     if(value === ''){
+      nowPosition = 0;
       return $keywords.style.display = 'none';
     };
+   
     fetch(
       `https://jf3iw5iguk.execute-api.ap-northeast-2.amazonaws.com/dev/api/cats/keywords?q=${value}`
     )
+    .catch((error) => {
+      console.log(error)
+    })
     .then((res) => res.json())
       .then((results) => {
-        // console.log(results)
-        
+        if(results.length === 0 ){
+          return $keywords.style.display = 'none';
+        }
         if (results.length !== 0) {
           $keywords.style.display = 'block';
             $keywords.innerHTML = results
             .map((name) => `<li>${name}</li>`)
             .join("");
+
+            const recommandText = document.querySelectorAll('li');
+            
             if(key === 'ArrowDown'){
-              const recommandText = document.querySelectorAll('li');
+              
               recommandText[nowPosition].className = 'active';
               choosedText = recommandText[nowPosition].innerText;
               nowPosition++;  
-              console.log(choosedText)
+            
             }
             if(key === 'ArrowUp'){
-              const recommandText = document.querySelectorAll('li');
-              if(nowPosition <= 1){
-                nowPosition = 0;
-                return  recommandText[nowPosition].className = 'active';  
+              if(nowPosition === 0){
+                return choosedText = value;
+              }else{
+                nowPosition--;
+                if(recommandText[nowPosition -1] === undefined){
+                  return choosedText = value;
+                }             
+                recommandText[nowPosition -1].className = 'active';   
+                choosedText = recommandText[nowPosition -1].innerText;
               }
-              nowPosition--;
-              recommandText[nowPosition -1].className = 'active';   
+
             }
         }
       }); 
   }
 });
 
+$keywords.addEventListener("click", function (e) {
+  console.log(e.target.innerHTML);
+  $keyword.value = e.target.innerHTML;
+  spinner.removeAttribute("hidden");
+  fetch(
+    `https://jf3iw5iguk.execute-api.ap-northeast-2.amazonaws.com/dev/api/cats/search?q=${$keyword.value}`
+  )
+    .then((res) => res.json())
+    .then((results) => {
+      spinner.setAttribute("hidden", "");
+      if (results.data) {
+        $keywords.style.display = 'none';
+        $searchResults.innerHTML = results.data
+          .map((cat) => `<article><img src="${cat.url}" /></article>`)
+          .join("");
+      }
+    }); 
+})
+
 
 window.onkeydown = function (e) {
   if(e.keyCode === 27){
     $keyword.value = '';
+    nowPosition = 0;
   }
 }
   
-
 
